957aab405b2c5fbe17408d60cebfec009e5739d6
MakkahMalik <makkahmalik86@gmail.com>
2025-01-16 03:05:59 -0700

    Create folders (Views, Models, Presenters) for MVP structure and add model classes.

---
+++ Models/ListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TODOweb.Models
+{
+    public class ListItem
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+        public int ItemPosition { get; set; }
+
+        public string ListColor { get; set; }
+
+        public DateTime CreateDt { get; set; }
+
+
+
+    }
+}
