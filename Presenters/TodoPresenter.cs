4cc8f5ed1a9d770662c88a128e44a74bcfa4e817
MakkahMalik <makkahmalik86@gmail.com>
2025-01-17 03:30:46 -0700

    files stage

---
+++ Presenters/TodoPresenter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI.WebControls;
+using TODOweb.Interfaces;
+using TODOweb.Models;
+using static TODOweb.Models.DatabaseHelper;
+
+namespace TODOweb.Presenters
+{
+     public class TodoPresenter
+    {
+
+        private readonly IListItemView _view;
+
+        private readonly DatabaseHelper model;
+
+        public TodoPresenter(IListItemView view)
+        {
+            _view = view;
+            model = new DatabaseHelper();
+            _view.OnAddItem += AddItem;
+            _view.OnDeleteItem += DeleteItem;
+            _view.OnMarkAsDone += MarkAsDone;
+            _view.OnUpdateRowColor += UpdateRowColor;
+           _view.OnUpdateItem += UpdateItem;
+            //_view.OnDeleteItem += DeleteItem;
+           
+            //_view.OnUpdatePositions += UpdatePositions;
+        }
+
+
+        public void LoadData()
+        {
+            var data = model.GetData();
+            _view.SetListData(data);
+        }
+        private void AddItem(object sender, ListEventArgs e)
+        {
+            model.AddItem(e.Description, e.ListColor);
+            LoadData();
+        }
+        private void DeleteItem(object sender, ListEventArgs e)
+        {
+            model.DeleteItem(e.ItemId);
+            LoadData();
+        }
+
+        private void MarkAsDone(object sender, ListEventArgs e)
+        {
+            model.MarkItemAsDone(e.ItemId);
+            LoadData();
+        }
+       
+        //private void UpdatePositions(object sender , ListEventArgs e)
+        //{
+        //    List<int> idsInOrder = new List<int> { e.ItemId};
+        //    model.UpdatePositions(idsInOrder);
+        //    LoadData();
+        //}
+        private void UpdateRowColor(object sender, ListEventArgs e)
+        {
+
+            model.UpdateRowColor(e.Action);
+            LoadData();
+        }
+
+        private void UpdateItem(object sender, ListEventArgs e)
+        {
+            model.UpdateItem(e.ItemId, e.Description);
+            LoadData();
+        }
+
+    }
+}
---
+++ Views/Test.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Optimization;
+using System.Data;
+using System.Web.Services;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Microsoft.Ajax.Utilities;
+using TODOweb.Interfaces;
+using TODOweb.Presenters;
+namespace TODOweb
+{
+    public partial class Test : System.Web.UI.Page , IListItemView
+    {
+        public event EventHandler<ListEventArgs> OnAddItem;
+        public event EventHandler<ListEventArgs> OnDeleteItem;
+        public event EventHandler<ListEventArgs> OnMarkAsDone;
+        public event EventHandler<ListEventArgs> OnUpdatePositions;
+        public event EventHandler<ListEventArgs> OnUpdateItem;
+        public event EventHandler<ListEventArgs> OnUpdateRowColor;
+        private readonly TodoPresenter _presenter;
+        public Test()
+        {
+            _presenter = new TodoPresenter(this);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                _presenter.LoadData();
+            }
+            else
+            {
+                string eventTarget = Request["__EVENTTARGET"];
+                string eventArgument = Request["__EVENTARGUMENT"];
+                if (Request["__EVENTTARGET"] == "DeleteRecord")
+                {
+                    int id = int.Parse(Request["__EVENTARGUMENT"]);
+                    DeleteRecord(id); // Call your existing DeleteRecord method
+                }
+                else if (eventTarget == "MarkAsDone")
+                {
+                    int id = int.Parse(Request["__EVENTARGUMENT"]);
+                    MarkAsDone(id);
+
+                }
+ 
+                else if (eventTarget == "UpdateRowColor")
+                {
+                    UpdateRowColor(eventArgument);
+                }
+
+            }
+
+            }
+
+        public void SetListData(DataTable data)
+        {
+            Repeater1.DataSource = data;
+            Repeater1.DataBind();
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string mode = hdnMode.Value;
+            string description = add_new.Text;
+
+            var args = new ListEventArgs { Description = description, ListColor = "red" };
+
+            if (mode == "Update")
+            {
+                args.ItemId = int.Parse(hdnItemId.Value);
+                OnUpdateItem?.Invoke(this, args);
+            }
+            else
+            {
+                OnAddItem?.Invoke(this, args);
+            }
+            add_new.Text = string.Empty;
+            hdnItemId.Value = "0";
+            hdnMode.Value = "Add";
+        }
+
+        protected void DeleteRecord(int id)
+        {
+            var args = new ListEventArgs { ItemId = id };
+            OnDeleteItem?.Invoke(this, args);
+        }
+
+        protected void MarkAsDone(int id)
+        {
+            var args = new ListEventArgs { ItemId = id };
+            OnMarkAsDone?.Invoke(this, args);
+        }
+
+        //protected void UpdatePositions(List<int> idsInOrder)
+        //{
+        //    var args = new ListEventArgs { ItemIds = idsInOrder };
+        //    OnUpdatePositions?.Invoke(this, args);
+        //}
+
+        protected void UpdateRowColor(string eventArgument)
+        {
+            var args = new ListEventArgs { Action = eventArgument };
+            OnUpdateRowColor?.Invoke(this, args);
+        }
+
+
+    }
+}
